dafdd56b0123ab94e65f51c6a17a53e7acae64ed
Martin Laursen <malaue4@mail.com>
2016-11-15 14:45:22 +0100

    Thanks Obama

--- src/HTML/article.php
+++ src/HTML/article.php
@@ -1,9 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>WITS Wiki</title>
+	<link rel="stylesheet" href="../CSS/Style.css">
+</head>
+<body>
+<div class="top">WITS-Wiki</div>
 
-$farticles = scandir(__DIR__ . "/../ARTICLES");
+<div class="left">
+	<?php
 
-foreach ($farticles as $farticle){
-	echo $farticle;
-}
+	$farticles = scandir(__DIR__ . "/../ARTICLES");
 
-?>
+	foreach ($farticles as $farticle){
+		if($farticle=='.' || $farticle=='..') continue;
+		echo "<a href='article.php?link=".$farticle."'>".$farticle."</a><br>";
+	}
+
+
+
+	?>
+</div>
+
+<div class="main">
+	<?php
+	if(isset($_GET['link'])){
+		echo file_get_contents(__DIR__ . "/../ARTICLES/".$_GET['link']);
+	}
+	?>
+</div>
+</body>
+</html>
